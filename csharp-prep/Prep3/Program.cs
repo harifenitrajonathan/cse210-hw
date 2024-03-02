7d407cf4ab6417a8154dec6aec99755dcdc3558a
harifenitra jonathan rajaonarison <harifenitra98nathan@gmail.com>
2024-03-02 17:23:32 +0300

    prep3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int x = -1;
+
+        while (x != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            x = int.Parse(Console.ReadLine());
+
+            if (magicNumber > x )
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < x)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } 
     }
 }
