a861600279965492e0ba32edb0580412d9d136e2
harifenitra jonathan rajaonarison <harifenitra98nathan@gmail.com>
2024-03-02 14:02:50 +0300

    prep2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage : ");
+        string answer = Console.ReadLine();
+
+        int x = int.Parse(answer);
+        string letter = "" ;
+
+        if (x >= 90)
+        {
+            letter = "A";
+        }
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+         else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine ($"Your grade is {letter}");
+
+        if (x >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else 
+        {
+            Console.WriteLine("Better loock next time !");
+        }
     }
 }
