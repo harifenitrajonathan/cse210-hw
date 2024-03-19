8256bd307487cf44e9ac9c8b239aa84610b6d27e
harifenitra jonathan rajaonarison <harifenitra98nathan@gmail.com>
2024-03-19 16:22:09 +0300

    Learning 04 done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment first = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(first.GetSummary());
+
+        MathAssignment second = new MathAssignment ("Roberto Rodriguez", "Fractions", " 7.3", "8-9");
+        Console.WriteLine(second.GetSummary());
+        Console.WriteLine(second.GetHomeworklist());
+    
+        WrittingAssignment third = new WrittingAssignment("Mary Waters", "European History", "The Cause of The World War II");
+        Console.WriteLine(third.GetSummary());
+        Console.WriteLine(third.GetWritingInformation());
     }
 }
